e58f12f27fa5bdaa3e4f37ee0f697f75312f68a1
Evan LV <evan@nine1000.com>
2021-08-01 12:47:52 +0200

    feat: add Item list & delete Fn

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,6 +1,21 @@
+import { useState } from "react";
+import { v4 as uuid } from "uuid";
+
 import Item from "./Item";
 
 const Form = () => {
+  const [dataArr, setDataArr] = useState([
+    { txt: "Household", id: uuid() },
+    { txt: "Walk the dog", id: uuid() },
+  ]);
+
+  const deleteTask = (id) => {
+    const filteredState = dataArr.filter((item) => {
+      return item.id !== id;
+    });
+    setDataArr(filteredState);
+  };
+
   return (
     <div className="m-auto px-4 col-12 col-sm-10 col-lg-6">
       <form className="mb-3">
@@ -13,9 +28,16 @@ const Form = () => {
 
       <h2>Task-list</h2>
       <ul className="list-group">
-        <Item />
-        <Item />
-        <Item />
+        {dataArr.map((item, index) => {
+          return (
+            <Item
+              txt={item.txt}
+              key={item.id}
+              id={item.id}
+              deleteFn={deleteTask}
+            />
+          );
+        })}
       </ul>
     </div>
   );
--- src/Components/Item.js
+++ src/Components/Item.js
@@ -1,8 +1,12 @@
-const Item = () => {
+const Item = (props) => {
+  const { txt, id, deleteFn } = props;
+
   return (
     <li className=" border d-flex justify-content-between align-items-center p-2 m-2">
-      <div className="p-3">Hello World</div>
-      <button className="btn btn-danger p-2 h-50">Delete</button>
+      <div className="p-3">{txt}</div>
+      <button className="btn btn-danger p-2 h-50" onClick={() => deleteFn(id)}>
+        Delete
+      </button>
     </li>
   );
 };
