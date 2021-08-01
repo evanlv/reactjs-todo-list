6040b874be46da05039a8337e87287912fd376a0
Evan LV <evan@nine1000.com>
2021-08-01 12:57:20 +0200

    feat: add task in todo list and clear input

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -8,6 +8,7 @@ const Form = () => {
     { txt: "Household", id: uuid() },
     { txt: "Walk the dog", id: uuid() },
   ]);
+  const [inputState, setInputState] = useState();
 
   const deleteTask = (id) => {
     const filteredState = dataArr.filter((item) => {
@@ -16,13 +17,38 @@ const Form = () => {
     setDataArr(filteredState);
   };
 
+  const addTodo = (e) => {
+    e.preventDefault();
+
+    const newArr = [...dataArr];
+    const newTodo = {};
+
+    newTodo.txt = inputState;
+    newTodo.id = uuid();
+
+    newArr.push(newTodo);
+    setDataArr(newArr);
+
+    setInputState("");
+  };
+
+  const linkedInput = (e) => {
+    setInputState(e);
+  };
+
   return (
     <div className="m-auto px-4 col-12 col-sm-10 col-lg-6">
-      <form className="mb-3">
+      <form className="mb-3" onSubmit={(e) => addTodo(e)}>
         <label htmlFor="todo" className="form-label mt-3">
           Task
         </label>
-        <input type="text" className="form-control" id="todo " />
+        <input
+          type="text"
+          className="form-control"
+          id="todo"
+          value={inputState}
+          onInput={(e) => linkedInput(e.target.value)}
+        />
         <button className="mt-2 btn-primary d-block">Send</button>
       </form>
 
